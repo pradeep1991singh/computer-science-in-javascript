36bcf73e97a396b81d7bf894927a7fbc17720a7a
pradeep singh <pradeep1991singh@gmail.com>
2019-08-21 13:36:32 +0530

    Update doubly-linked-list.js

--- data-structures-in-javascript/doubly-linked-list.js
+++ data-structures-in-javascript/doubly-linked-list.js
@@ -1,3 +1,6 @@
+// A Doubly Linked List (DLL) contains an extra pointer, typically called previous pointer, 
+// together with next pointer and data which are there in singly linked list.
+
 function Node(data) {
   this.data = data;
   this.previous = null;
