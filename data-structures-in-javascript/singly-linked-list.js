d1f2e8738de3839d671fe9436f33a3f9c272f783
pradeep singh <pradeep1991singh@gmail.com>
2019-08-21 13:31:12 +0530

    Update singly-linked-list.js

--- data-structures-in-javascript/singly-linked-list.js
+++ data-structures-in-javascript/singly-linked-list.js
@@ -1,3 +1,7 @@
+// A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations.
+// In simple words, a linked list consists of nodes where each node contains a data field and a reference(link)
+// to the next node in the list
+
 function Node(data) {
   this.data = data;
   this.next = null;
