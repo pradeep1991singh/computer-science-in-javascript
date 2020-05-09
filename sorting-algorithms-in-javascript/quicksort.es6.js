f675047f860da1066682ff49b11716eaee3c3af5
pradeep singh <pradeep1991singh@gmail.com>
2020-05-09 23:57:02 +0530

    Add spec to quick sort es6

--- sorting-algorithms-in-javascript/quicksort.es6.js
+++ sorting-algorithms-in-javascript/quicksort.es6.js
@@ -1,3 +1,27 @@
+/******************************************************
+** Quick sort,
+** Quick sort is an efficient sorting algorithm. Developed by British computer scientist Tony Hoare in 1959 and published in 1961, it is still a commonly used algorithm for sorting. 
+** When implemented well, it can be about two or three times faster than its main competitors, merge sort and heap sort.
+** Quick sort is a divide-and-conquer algorithm.
+** It works by selecting a 'pivot' element from the array and partitioning the other elements into two sub-arrays, according to whether they are less than or greater than the pivot. 
+** The sub-arrays are then sorted recursively. This can be done in-place, requiring small additional amounts of memory to perform the sorting.
+**
+** Spec:
+** Worst complexity: n^2
+** Average complexity: n*log(n)
+** Best complexity: n*log(n)
+** Space complexity: n
+** Method: Partitioning
+** Stable: No
+** Class: Comparison sort
+**
+** For an array, in which partitioning leads to unbalanced subarrays, 
+** to an extent where on the left side there are no elements,
+** with all the elements greater than the pivot, hence on the right side.
+** And if keep on getting unbalanced subarrays, then the running time is the worst case, which is O(n2)
+** Where as if partitioning leads to almost equal subarrays, then the running time is the best, with time complexity as O(n*log n).
+******************************************************/
+
 // array to sort
 const array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 
