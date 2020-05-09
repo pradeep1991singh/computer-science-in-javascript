a41baaa8e70ac3766e18c0561d89c4c29a1bd119
pradeep singh <pradeep1991singh@gmail.com>
2020-05-10 00:26:14 +0530

    Add spec for shell sort es6

--- sorting-algorithms-in-javascript/shellsort.es6.js
+++ sorting-algorithms-in-javascript/shellsort.es6.js
@@ -1,3 +1,25 @@
+/******************************************************
+** Shell sort,
+** Shell sort, also known as Shell sort or Shell's method, is an in-place comparison sort. 
+** It can be seen as either a generalisation of sorting by exchange or sorting by insertion. 
+** The method starts by sorting pairs of elements far apart from each other, 
+** then progressively reducing the gap between elements to be compared.
+**
+** Spec:
+** Inventor: Donald Shell
+** Worst complexity: Depends on gap sequence
+** Average complexity: n*log(n)^2 or n^(3/2)
+** Best complexity: n
+** Space complexity: 1
+** Method: Insertion
+** Stable: No
+** Class: Comparison sort
+**
+** Selection Sort requires two nested for loops to complete itself, 
+** one for loop is in the function selectionSort, 
+** and inside the first loop we are making a call to another function indexOfMinimum, which has the second(inner) for loop.
+******************************************************/
+
 // array to sort
 const array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 
